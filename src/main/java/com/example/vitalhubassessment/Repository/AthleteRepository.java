d041404a749a12319a3192c4f109ee679e2fa4c9
Umanda-t <57774048+Umanda-t@users.noreply.github.com>
2023-03-07 23:36:49 +0530

    Update AthleteRepository

--- src/main/java/com/example/vitalhubassessment/Repository/AthleteRepository.java
+++ src/main/java/com/example/vitalhubassessment/Repository/AthleteRepository.java
@@ -8,8 +8,10 @@ import java.util.List;
 
 public interface AthleteRepository extends JpaRepository<Athlete, Long> {
 
-    @Query("SELECT a FROM Athlete a WHERE a.firstname=:name OR a.lastname=:name OR a.country=:country OR a.gender=:gender OR a.event=:event")
+    @Query("SELECT a FROM Athlete a WHERE (a.firstname=:name OR a.lastname=:name) AND a.country=:country AND a.gender=:gender AND a.event=:event")
     public List<Athlete> findByKeyword(String name,String country, String gender, String event);
 
+//    @Query("SELECT a FROM Athlete a WHERE a.firstname=:name OR a.lastname=:name OR a.country=:country OR a.gender=:gender OR a.event=:event")
+//    public List<Athlete> findByKeyword(String name,String country, String gender, String event);
 
 }
